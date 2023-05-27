50649f571cc2950a6d23f025f28112da2909c1a1
juanchixd <juanchibgonzalez@gmail.com>
2023-05-26 23:47:47 -0300

    analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.module.scss";
 import BaseLayout from "./components/BaseLayout";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+
+<Analytics mode={"production"} />;
 
 // let docTitle = document.title;
 
